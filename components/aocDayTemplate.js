064551ec57fc7d1b3e8e010ea7b80b6e192d0305
creaseaj <adam@coreblue.co.uk>
2021-07-29 09:01:02 +0100

    templates and css updated

---
+++ components/aocDayTemplate.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { useState } from 'react'
+import styles from '../styles/Home.module.css'
+
+const AocDayTemplate = (dayFunction) => {
+  const [answer, setAnswer] = useState(null)
+  const [input, setInput] = useState(null)
+  return (
+    <div className="bg-purple-200 m-20 rounded-md p-10">
+      <div className="flex flex-col items-center justify-center text-2xl ">
+        <h1>Day 1</h1>
+        <button className="bg-purple-700 px-10 py-5 m-5 w-56 rounded-md text-white" onClick={() => setAnswer(input)}>Run Day</button>
+      </div>
+      <div className="grid grid-cols-2 h-72 gap-10">
+        <div >
+          <textarea onChange={(v) => setInput(v.target.value)} className="w-full h-full rounded-md resize-none p-5" />
+        </div>
+        <div>
+          <textarea value={answer} className="w-full h-full rounded-md resize-none p-5" />
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
+export default AocDayTemplate
